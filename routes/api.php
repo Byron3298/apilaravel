3feb7272acc83db25b73f27d9545678fae8a3b7d
Byron3298 <byron.barrera@istvidanueva.edu.ec>
2024-11-22 09:19:43 -0500

    d

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('persona','App\Http\Controllers\personacontroller@getpersona');
+Route::get('personas/(id)','App\Http\Controllers\personacontroller@getpersonaid');
+Route::post('addpersona','App\Http\Controllers\personacontroller@insertpersona');
+Route::put('updatepersona','App\Http\Controllers\personacontroller@updatepersona');
+Route::delete('deletepersona','App\Http\Controllers\personacontroller@deletepersona');
